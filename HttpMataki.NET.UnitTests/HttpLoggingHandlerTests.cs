bb02bf067411cf00006c0a5f75a70d5415ff053d
yzk <yangzhongke8@gmail.com>
2025-09-19 06:23:02 +1200

    Remove unused usings and clean up solution file

--- HttpMataki.NET.UnitTests/HttpLoggingHandlerTests.cs
+++ HttpMataki.NET.UnitTests/HttpLoggingHandlerTests.cs
@@ -1,11 +1,4 @@
-using Xunit;
-using HttpMataki.NET;
-using System;
-using System.IO;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Collections.Generic;
+namespace HttpMataki.NET.UnitTests;
 
 public class HttpLoggingHandlerTests
 {
